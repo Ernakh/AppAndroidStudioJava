050e13837521a6287bfb3dc0986c13a652e30088
Fabrício <fabriciotonettolondero@gmail.com>
2025-04-07 11:12:28 -0300

    primeira thread

--- app/src/main/java/com/ernakh/aplicativoaula/MainActivity.java
+++ app/src/main/java/com/ernakh/aplicativoaula/MainActivity.java
@@ -136,6 +136,12 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
+    public void carregarActivityThreads(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, ThreadsActivity.class);
+        startActivity(intent);
+    }
+
     public void carregarActivitySqlLite(View view)
     {
         Intent intent = new Intent(MainActivity.this, SqlLiteActivity.class);
