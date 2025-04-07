c3745669698fd4694cf0bb5612076c37d273f34b
Fabrício <fabriciotonettolondero@gmail.com>
2025-04-07 15:38:14 -0300

    backgroundTasks finalizado

--- app/src/main/java/com/ernakh/aplicativoaula/ThreadsActivity.java
+++ app/src/main/java/com/ernakh/aplicativoaula/ThreadsActivity.java
@@ -1,15 +1,27 @@
 package com.ernakh.aplicativoaula;
 
+import android.app.IntentService;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,7 +32,7 @@ public class ThreadsActivity extends AppCompatActivity {
 
     TextView texto;
     ProgressBar progresso;
-    Button botao;
+    Button botao, btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,15 @@ public class ThreadsActivity extends AppCompatActivity {
         botao.setOnClickListener(view -> {
             new MinhaTask().execute();
         });
+
+        btnIniciar = findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MeuServico.class);
+            startService(intent);
+        });
+
+        Intent intent = new Intent(this, MinhaIntentService.class);
+        this.startService(intent);
     }
 
 
@@ -78,6 +99,7 @@ public class ThreadsActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
+            progresso.setProgress(values[0]);
             texto.setText("Progresso: " + values[0] + "%");
         }
 
@@ -86,6 +108,76 @@ public class ThreadsActivity extends AppCompatActivity {
             super.onPostExecute(resultado);
             progresso.setVisibility(View.GONE);
             texto.setText(resultado);
+        }
+    }
+
+    //deixar ela static ou tornar uma classe externa
+    public static class MeuServico extends Service {
+
+        private Handler handler = new Handler();
+        private int segundos = 0;
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+
+            segundos = 0;
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.i("Service", "run...");
+                    segundos++;
+                    if (segundos < 10) {
+                        handler.postDelayed(this, 1000);
+                    } else {
+                        Log.i("Service", "10 segundos depois...");
+                        Toast.makeText(getApplicationContext(), "10 segundos se passaram!", Toast.LENGTH_LONG).show();
+                        stopSelf();
+                    }
+                }
+            }, 1000);
+
+
+            return Service.START_NOT_STICKY;
+        }
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+    }
+
+    //deixar ela static ou tornar uma classe externa
+    public static class MinhaIntentService extends IntentService {
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Log.i("teste", "Intent Service criada");
+
+        }
+
+        @Override
+        protected void onHandleIntent(Intent arg0) {
+            Log.i("teste", "IntentService iniciado");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(() -> Toast.makeText(getApplicationContext(), "Tarefa finalizada!", Toast.LENGTH_SHORT).show());
+        }
+
+        public MinhaIntentService() {
+            super("MinhaIntentService");
         }
     }
 }
